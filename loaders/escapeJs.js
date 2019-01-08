a51a31e9186dac0935f49276e690ee798cf917fb
karak <karak97@gmail.com>
2019-01-08 17:07:24 +0900

    remove unused files.

--- loaders/escapeJs.js
+++
@@ -1,9 +0,0 @@
-function escapeJs(source) {
-  return source
-    .replace(/\\/g, "\\\\")
-    .replace(/\"/g, '\\"')
-    .replace(/\'/g, "\\'")
-    .replace(/\n/g, '\\n')
-}
-
-module.exports = escapeJs;
